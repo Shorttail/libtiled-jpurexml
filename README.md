175373661087f51860218b6f6f1908465f502c32
devnewton <devnewton@bci.im>
2014-01-23 11:27:48 +0100

    add unit test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 libtiled-jpurexml is a java library to parse [Tiled](http://www.mapeditor.org) tmx maps.
 
 It use [jpurexml](https://github.com/asilvestre/jpurexml), a pure java xml parser in order to be usable with [gwt](http://www.gwtproject.org/) compiler.
+
+Usage sample: [TestTmxLoader.java](src/test/java/im/bci/test/TestTmxLoader.java)
---
+++ src/test/java/im/bci/test/TestTmxLoader.java
@@ -0,0 +1,81 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2014 devnewton <devnewton@bci.im>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+package im.bci.test;
+
+import im.bci.tmxloader.TmxLoader;
+import im.bci.tmxloader.TmxMap;
+import im.bci.tmxloader.TmxTile;
+import im.bci.tmxloader.TmxTileset;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ *
+ * @author devnewton
+ */
+public class TestTmxLoader {
+
+    @Test
+    public void simpleTmx() throws IOException {
+        final File mapFile = new File("test-data/desert.tmx");
+        final File mapParentDir = mapFile.getParentFile().getCanonicalFile();
+        TmxLoader loader = new TmxLoader();
+        TmxMap map = new TmxMap();
+        loader.parseTmx(map, loadText(mapFile));
+        for (TmxTileset tileset : map.getTilesets()) {
+            File tilesetParentDir;
+            if (null != tileset.getSource()) {
+                final File tilesetFile = new File(mapParentDir, tileset.getSource());
+                tilesetParentDir = tilesetFile.getParentFile().getCanonicalFile();
+                loader.parseTsx(map, tileset, loadText(tilesetFile));
+            } else {
+                tilesetParentDir = mapParentDir;
+            }
+            if (null != tileset.getImage()) {
+                tileset.getImage().setSource(new File(tilesetParentDir, tileset.getImage().getSource()).getCanonicalPath());
+            }
+            for (TmxTile tile : tileset.getTiles()) {
+                tile.getFrame().getImage().setSource(new File(tilesetParentDir, tile.getFrame().getImage().getSource()).getCanonicalPath());
+            }
+        }
+        loader.decode(map);
+
+        Assert.assertEquals(40, map.getWidth());
+        Assert.assertEquals(40, map.getHeight());
+        Assert.assertEquals(32, map.getTilewidth());
+        Assert.assertEquals(32, map.getTileheight());
+        Assert.assertEquals(1, map.getLayers().size());
+    }
+
+    private String loadText(File f) throws IOException {
+        try (InputStream is = new FileInputStream(f); Scanner s = new Scanner(is, "UTF-8").useDelimiter("\\Z")) {
+            return s.next();
+        }
+    }
+}
